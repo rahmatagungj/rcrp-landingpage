324d15a4504ad7ee8519f6cd3b029db29db6e06b
Rahmat Agung Julians <rahmatagungj@Gmail.com>
2021-08-02 16:41:29 +0700

    add statistic page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Statistic from "./pages/Statistic";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/statistic" exact component={Statistic} />
       </Switch>
     </Router>
   );
