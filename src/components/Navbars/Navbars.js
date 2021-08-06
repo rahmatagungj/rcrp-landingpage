05f0c8c286c5bff964aac6f3117ba8c25d4ac8f9
Albert Ramadhan Van Wijk <albertramadhanvanwijk@gmail.com>
2021-08-06 23:35:04 +0700

    navbar mobile version

--- src/components/Navbars/Navbars.js
+++ src/components/Navbars/Navbars.js
@@ -15,6 +15,15 @@ const Navbars = () => {
               Republic City
               </a>
           </div>
+
+          <div class="flex md:hidden">
+              <button type="button" class="text-white dark:text-gray-200 hover:text-gray-400 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                  <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                      <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+                  </svg>
+              </button>
+          </div>
+
         </div>
         <div className="lg:flex flex-grow items-center" id="example-navbar-warning">
           <ul className="flex flex-col lg:flex-row list-none ml-auto">
