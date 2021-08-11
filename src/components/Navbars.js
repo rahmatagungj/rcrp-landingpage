e25b25adae684dab09896554824f7366b571ae6b
Naufal Akbar <kbrnugroho@gmail.com>
2021-08-11 19:44:02 +0700

    change bg color nav hidden

--- src/components/Navbars.js
+++ src/components/Navbars.js
@@ -71,11 +71,11 @@ const Navbars = () => {
               </button>
               {isMenuOpen && (
                 <div className="absolute top-0 left-0 w-full z-10">
-                  <div className="p-5 bg-green-600 border rounded shadow-sm">
+                  <div className="p-5 bg-white border rounded shadow-sm">
                     <div className="flex items-center justify-between mb-4">
                       <div>
                         <Link to="/" aria-label="Company" title="Company" className="inline-flex items-center">
-                          <span className="ml-2 text-xl font-bold tracking-wide text-white">Republic City</span>
+                          <span className="ml-2 text-xl font-bold tracking-wide text-black">Republic City</span>
                         </Link>
                       </div>
                       <div>
@@ -85,7 +85,7 @@ const Navbars = () => {
                           className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                           onClick={() => setIsMenuOpen(false)}
                         >
-                          <svg className="w-5 text-white" viewBox="0 0 24 24">
+                          <svg className="w-5 text-black" viewBox="0 0 24 24">
                             <path
                               fill="currentColor"
                               d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
@@ -97,27 +97,27 @@ const Navbars = () => {
                     <nav>
                       <ul className="space-y-4">
                         <li>
-                          <Link to="/" className="font-medium tracking-wide text-white transition-colors duration-200 hover:text-primary-focus">
+                          <Link to="/" className="font-medium tracking-wide text-black transition-colors duration-200 hover:text-primary-focus">
                             Home
                           </Link>
                         </li>
                         <li>
-                          <Link to="/forum" className="font-medium tracking-wide text-white transition-colors duration-200 hover:text-primary-focus">
+                          <Link to="/forum" className="font-medium tracking-wide text-black transition-colors duration-200 hover:text-primary-focus">
                             Forum
                           </Link>
                         </li>
                         <li>
-                          <Link to="/donasi" className="font-medium tracking-wide text-white transition-colors duration-200 hover:text-primary-focus">
+                          <Link to="/donasi" className="font-medium tracking-wide text-black transition-colors duration-200 hover:text-primary-focus">
                             Donasi
                           </Link>
                         </li>
                         <li>
-                          <Link to="/status" className="font-medium tracking-wide text-white transition-colors duration-200 hover:text-primary-focus">
+                          <Link to="/status" className="font-medium tracking-wide text-black transition-colors duration-200 hover:text-primary-focus">
                             Status
                           </Link>
                         </li>
                         <li>
-                          <Link to="/statistik" className="font-medium tracking-wide text-white transition-colors duration-200 hover:text-primary-focus">
+                          <Link to="/statistik" className="font-medium tracking-wide text-black transition-colors duration-200 hover:text-primary-focus">
                             Statistik
                           </Link>
                         </li>
