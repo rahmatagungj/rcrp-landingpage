4ed5e9de02b012520f98ee2dd97be3b3fe31f1ed
Naufal Akbar <kbrnugroho@gmail.com>
2021-08-11 18:52:34 +0700

    fix card title

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -106,7 +106,7 @@ function Home() {
               <div className="p-2 xl:w-1/4 md:w-1/2 w-full">
                 <div className="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
                   <span className="bg-gray-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">MINIMUM</span>
-                  <h2 className="text-sm tracking-widest title-font mb-1 font-medium">SPEK</h2>
+                  <h2 className="text-sm tracking-widest title-font mb-1 font-medium">PAKET</h2>
                   <h1 className="text-4xl font-medium text-black pb-4 mb-4 border-b border-gray-200 leading-none">HEMAT</h1>
                   <p className="flex items-center text-gray-600 mb-2">
                     <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
@@ -169,7 +169,7 @@ function Home() {
               <div className="p-2 xl:w-1/4 md:w-1/2 w-full">
                 <div className="h-full p-6 rounded-lg border-2 border-green-500 flex flex-col relative overflow-hidden">
                   <span className="bg-green-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">REKOMENDASI</span>
-                  <h2 className="text-sm tracking-widest title-font mb-1 font-medium">SPEK</h2>
+                  <h2 className="text-sm tracking-widest title-font mb-1 font-medium">PAKET</h2>
                   <h1 className="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
                     <span className="text-4xl font-semibold">SULTAN</span>
                   </h1>
